da3b56f96b8f0af3dc2f1bbf92c902eb0a5258ba
unknown <evermore2930@gmail.com>
2022-07-23 16:39:08 -0400

    creating private instance

---
+++ instancepub.tf
@@ -0,0 +1,8 @@
+
+resource "aws_instance" "instancepri" {
+    instance_type = t2_micro
+    ami = ami-0cff7528ff583bf9a
+    subnet_id = aws_subnet.priv_subnet.id
+    key_name = Cloudkey
+   availability_zone = "us-east-1b"
+}
