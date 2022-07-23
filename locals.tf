6b8c68dfda48a2fe2d639f94fe34fce178d8ad5d
kunskytested <rsk.kunle@yahoo.com>
2022-07-23 14:09:48 -0500

    removed the line that was not needed

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 
 locals {
   vpc_id = aws_vpc.cloudpros_vpc.id
-  create_vpc = var.create_vpcs
 }
--- variables.tf
+++ variables.tf
@@ -4,8 +4,3 @@ variable "cidr_block" {
   type = string
   default = "10.0.0.0/16"
 }
-variable "create_vpc" {
-  type = bool
-  default = true
-  
-}
