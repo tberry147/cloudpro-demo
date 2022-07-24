a576762ecdff37ed35d3d26ed1d581f7dca6d38e
unknown <evermore2930@gmail.com>
2022-07-23 20:33:52 -0400

    new ec2

--- instancepub.tf
+++ instancepub.tf
@@ -1,8 +1,10 @@
 
 resource "aws_instance" "instancepri" {
-    instance_type = t2_micro
+    instance_type = t2.micro
     ami = ami-0cff7528ff583bf9a
     subnet_id = aws_subnet.priv_subnet.id
-    key_name = Cloudkey
+    key_name = "Cloudkey"
    availability_zone = "us-east-1b"
 }
+
+
